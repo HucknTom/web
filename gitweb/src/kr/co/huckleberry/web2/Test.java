92f59670e56cad0e391bb3e55c5d8ff71cc134e1
박성희 <huck0505@feelingk.com>
2018-07-03 14:44:08 +0900

    ?

--- gitweb/src/kr/co/huckleberry/web2/Test.java
+++ gitweb/src/kr/co/huckleberry/web2/Test.java
@@ -27,7 +27,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at1111: ").append(request.getContextPath());
+		response.getWriter().append("Served at1111: kimkim babu").append(request.getContextPath());
 	}
 
 	/**
